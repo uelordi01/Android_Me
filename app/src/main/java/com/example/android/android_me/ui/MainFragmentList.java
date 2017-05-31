ea1ea9f018faf1e430a547d41052868ee628b26d
uelordi <elordiae@gmail.com>
2017-05-31 23:04:25 +0200

    adding list view of main activity

---
+++ app/src/main/java/com/example/android/android_me/ui/MainFragmentList.java
@@ -0,0 +1,30 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by uelordi on 31/05/17.
+ */
+
+public class MainFragmentList extends Fragment{
+    GridView mGView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        mGView = (GridView) rootView.findViewById(R.id.gv_puppet_list);
+        MasterListAdapter mAdapter = new MasterListAdapter(getActivity().getApplicationContext(),
+                                                            AndroidImageAssets.getAll());
+        mGView.setAdapter(mAdapter);
+        return rootView;
+    }
+
+}
